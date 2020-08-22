a3c6ede4870b3f77bdceea4933e84829455d917a
Gerald Lou Berzuela <berzuelagerald3348@gmail.com>
2020-08-22 12:55:21 -0700

    Added problem 1512 and updated README

---
+++ GoodPairs.rb
@@ -0,0 +1,12 @@
+# @param {Integer[]} nums
+# @return {Integer}
+
+def num_identical_pairs(nums)
+    result = 0
+    for i in 0...nums.length
+        for j in i+1...nums.length
+            result = nums[i]==nums[j] ? result+=1 : result
+        end
+    end
+    result
+end
